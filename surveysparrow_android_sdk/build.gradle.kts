99be4ff37313360a6901dc8aa07b3fa2cede4735
Gokulkrishna-Raju <gokulkrishna.raju@surveysparrow.com>
2024-06-20 20:54:35 +0530

    Fix

--- surveysparrow_android_sdk/build.gradle.kts
+++ surveysparrow_android_sdk/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
 
 }
 
